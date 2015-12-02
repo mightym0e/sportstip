ffdf2020831e24ccfbf71fd00dde15864bfd5b4c
Arian Meidow <arian@meidow.de>
2015-12-02 20:15:56 +0100

    I'm dumb

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,12 +1,19 @@
 describe "the signin process", :type => :feature do
   before :each do
     @email = 'admin@admin.de'
+    @password = 'admin'
+    User.create(
+      username: 'admin',
+      password: @password,
+      email: @email,
+      isadmin: true
+    )
   end
 
   it 'signs me in' do
     visit login_path
     fill_in 'email', :with => @email
-    fill_in 'password', :with => 'admin'
+    fill_in 'password', :with => @password
     click_button 'Log In'
     expect(page).to have_content 'Logged in as ' + @email
   end
