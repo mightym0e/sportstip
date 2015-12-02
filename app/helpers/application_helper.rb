51fec0dd81c7c67a450a32d836bb71d9edd93a24
Moritz <moritz.liepe@moejo.net>
2015-12-02 19:59:58 +0100

    Login Fehler Message, Users Index Test korrigiert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
 	  link_to title, {:sort => column, :direction => direction}, {:class => css_class}
 	end
 
+	def flash_class(level)
+	    case level
+	        when 'notice' then "alert alert-info"
+	        when 'success' then "alert alert-success"
+	        when 'error' then "alert alert-warning"
+	        when 'alert' then "alert alert-warning"
+	    end
+	end
+
 end
